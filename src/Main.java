d861d5da5f8225e166dc4f221a9e152637e8e721
SturessonAdam <Adam.sturesson@live.se>
2023-12-07 11:34:47 +0100

    La till en metod för välja ord att spela med

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,17 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!2");
-        System.out.println("test");
+import java.util.Scanner;
 
+public class Main {
+    private static final Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        System.out.println("HangMan");
+
+    }
+
+
+    //metod för att välja ord att spela med
+    private static String GameWord() {
+        System.out.print("Ange vilket ord du vill spela med: ");
+        String word = scanner.nextLine().toLowerCase();
+        return word;
     }
 }
